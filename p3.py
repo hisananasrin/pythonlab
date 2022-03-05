b0068f52b0d174bec31c95503f2d079d3ac451e2
hisananasrin <hisananasrin3@gmail.com>
2022-03-04 21:28:54 -0800

    hiii

---
+++ p3.py
@@ -0,0 +1,10 @@
+# Python program to print positive Numbers in a List
+list1 = [11, -21, 0, 45, 66, -93]
+for num in list1:
+    if num>=0:
+        print(num,end="")
+# Python program to print Square of Numbers
+print()
+list1= [1,2,3,4,5]
+newlist=[s*s for s in list1]
+print(newlist)
